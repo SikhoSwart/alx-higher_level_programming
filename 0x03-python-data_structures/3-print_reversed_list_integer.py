ad7753c641ef60ff94cc116075f9dab582a0eb3c
SikhoSwart <sikhoswart@gmail.com>
2023-07-15 07:24:09 -0700

    Fixed formatting

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    length = len(my_list)
-    idx = length + 1
-    for i in range(-1, -idx, -1):
-        print("{:d}", my_list[i])
+    my_list.reverse()
+    for i in my_list:
+        print("{:d}".format(i))
