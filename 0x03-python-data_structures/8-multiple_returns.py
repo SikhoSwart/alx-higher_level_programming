a43367c9edc0fb5c6c52d1a90f27c2e551595ac7
SikhoSwart <sikhoswart@gmail.com>
2023-07-16 06:35:59 -0700

    Fixed Task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,5 +3,6 @@ def multiple_returns(sentence):
     first = sentence[0]
     if len(sentence) == 0:
         t = (0, None)
-    t = (len(sentence), first)
+    else:
+        t = (len(sentence), first)
     return t
