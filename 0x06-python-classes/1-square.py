d0978677cfbdf9939730ce86fe805f5649350b8d
SikhoSwart <sikhoswart@gmail.com>
2023-08-01 07:50:59 -0700

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Write a class Square that defines a square by: (based on 0-square.py)."""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size):
+        """
+        Private instance attribute: size
+        """
+        self.__size = size
