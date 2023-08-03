8083d960c6cbe0911fd6c3abfe8d58e402c9753c
SikhoSwart <sikhoswart@gmail.com>
2023-08-03 13:22:22 -0700

    Added task 0 and Tests

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """Adds two integers."""
+    if type(a) == float:
+        a = int(a)
+    elif type(a) != int:
+        raise TypeError("a must be an integer")
+    if type(b) == float:
+        b = int(b)
+    elif type(b) != int:
+        raise TypeError("b must be an integer")
+    return (a + b)
