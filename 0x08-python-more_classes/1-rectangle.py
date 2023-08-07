5e3932c17799caf905efb953ba6bf20850ccfc88
SikhoSwart <sikhoswart@gmail.com>
2023-08-07 04:23:01 -0700

    Task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,8 +6,8 @@ class Rectangle:
     """Defines a rectangle."""
     def __init__(self, width=0, height=0):
         """Private instance attribute: width and height."""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
