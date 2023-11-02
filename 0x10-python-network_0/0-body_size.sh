72f00b4c30cc9f278d6a682f02d4f42ea3518479
SikhoSwart <sikhoswart@gmail.com>
2023-11-02 12:36:20 -0700

    Task 1 and 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a request it, displays the size of the body of response
-curl -Is "$1" | grep "Content-Length" | cut -d "" -f2
+curl -Is "$1" | grep "Content-Length" | cut -d " " -f2
