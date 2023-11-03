ca3d536889f8387f261497685cc04d418048b9cf
SikhoSwart <sikhoswart@gmail.com>
2023-11-03 07:55:12 -0700

    Task 8 and 9

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""script that takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id.
+"""
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+if __name__ == "__main__":
+    url = "https://api.github.com/user"
+    username = sys.argv[1]
+    password = sys.argv[2]
+    response = requests.get(url, auth=HTTPBasicAuth(username, password))
+    jobj = response.json()
+    print(jobj.get("id"))
